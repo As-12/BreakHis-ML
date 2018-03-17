7e66c5f44e8a96d58db76b81dd356826480c0d00
As-12 <As-12@github.com>
2018-03-17 23:13:29 +0000

    Experiment 7

--- experiment5/upload_result.sh
+++ experiment5/upload_result.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-gdrive upload "$1_experiment4c-$2-inception3_loss.pdf"
-gdrive upload "$1_experiment4c-$2-inception3_accuracy.pdf"
-gdrive upload "$1_experiment4c-$2-inception3_train.pickle"
-gdrive upload "$1_experiment4c-$2-inception3_CM.pdf"
-gdrive upload "$1_experiment4c-$2-inception3-weights.hdf5"
-gdrive upload "$1_experiment4c-$2-inception3_eval.txt"
+gdrive upload "$1_experiment5b-$2-inception3_loss.pdf"
+gdrive upload "$1_experiment5b-$2-inception3_accuracy.pdf"
+gdrive upload "$1_experiment5b-$2-inception3_train.pickle"
+gdrive upload "$1_experiment5b-$2-inception3_CM.pdf"
+gdrive upload "$1_experiment5b-$2-inception3-weights.hdf5"
+gdrive upload "$1_experiment5b-$2-inception3_eval.txt"
 gdrive upload "rescaled-$2-images.pdf"
 
 
--- experiment6/upload_result.sh
+++ experiment6/upload_result.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
-gdrive upload "$1_experiment4c-$2-inception3_loss.pdf"
-gdrive upload "$1_experiment4c-$2-inception3_accuracy.pdf"
-gdrive upload "$1_experiment4c-$2-inception3_train.pickle"
-gdrive upload "$1_experiment4c-$2-inception3_CM.pdf"
-gdrive upload "$1_experiment4c-$2-inception3-weights.hdf5"
-gdrive upload "$1_experiment4c-$2-inception3_eval.txt"
+gdrive upload "$1_experiment6c-$2-inception3_loss.pdf"
+gdrive upload "$1_experiment6c-$2-inception3_accuracy.pdf"
+gdrive upload "$1_experiment6c-$2-inception3_train.pickle"
+gdrive upload "$1_experiment6c-$2-inception3_CM.pdf"
+gdrive upload "$1_experiment6c-$2-inception3-weights.hdf5"
+gdrive upload "$1_experiment6c-$2-inception3_eval.txt"
 gdrive upload "rescaled-$2-images.pdf"
-
-
+gdrive upload "$3x$3-segmented--$2--images.pdf" 
